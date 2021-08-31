2d60cad2214ac810c535efa1b690d650d8ba1d69
farui.yu <farui.yu@kingstarfintech.com>
2021-08-31 15:42:15 +0800

    跨域测试的支持

--- src/main/resources/web/static/js/follower.js
+++ src/main/resources/web/static/js/follower.js
@@ -8,17 +8,49 @@ layui.define(['layer', 'form', 'table'], function(exports){
   exports('follower', {
 
         init : function (){
-            table.render({
-            elem: '#follower'
-            ,url:'/follower/list'
-            ,method:'get'
-            ,cellMinWidth: 80 //全局定义常规单元格的最小宽度，layui 2.2.1 新增
-            ,cols: [[
-              {field:'ip', width:'30%', title: 'ip地址',align:'center'}
-              ,{field:'port', width:'30%', title: '端口',align:'center'}
-              ,{field:'state', width:'30%', title: '状态',align:'center'}
-            ]]
-            });
+
+            // 测试跨域
+            layui.$.ajax(
+                {
+                    url:'http://localhost:8777/rule-template/create',
+                    type:'post',
+                    data:{
+                        "templateNo":"111"
+                    },
+                    success:function(data){
+                        console.log("sucess");
+                    },
+                    error:function(data){console.log("error");}
+                }
+            );
+
+//            layui.$.ajax(
+//                {
+//                    url:'/test',
+//                    type:'post',
+//                    headers: {
+//                        'Content-Type': 'text/html;',
+//                        'test':'<script>alert(123)</script>'
+//                    },
+//                    data: "<script>alert(123)</script>",
+//                    success:function(data){
+//                        console.log("sucess");
+//                    },
+//                    error:function(data){console.log("error");}
+//                }
+//            );
+
+//            table.render({
+//            elem: '#follower'
+//            ,url:'/follower/list'
+//            ,method:'get'
+//            ,cellMinWidth: 80 //全局定义常规单元格的最小宽度，layui 2.2.1 新增
+//            ,cols: [[
+//              {field:'ip', width:'30%', title: 'ip地址',align:'center'}
+//              ,{field:'port', width:'30%', title: '端口',align:'center'}
+//              ,{field:'state', width:'30%', title: '状态',align:'center'}
+//            ]]
+//            });
         }
   });
 });
